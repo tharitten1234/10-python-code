9c6e7bad74bbb6135ccf2d3d4e0b5a1517fe2ae1
tharitten1234 <taritten@gmail.com>
2025-09-30 15:10:12 +0700

    not done week 10

--- assignments/week10/personal-validator.py
+++ assignments/week10/personal-validator.py
@@ -28,3 +28,67 @@ Age Group: Young Adult (18-30)
 Phone: +91-9876543210
 
 """
+
+print("=== PERSONAL INFORMATION VALIDATOR ===")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+phone_number = input("Enter your phone number: ")
+
+#name = "John Doe"
+#age = "25"
+#phone_number = "9876543210"
+
+
+
+# validate name
+nameFlag =True
+
+for char in name:
+    print(char, char.isalpha())
+    if char.isalpha() or char !=" ":
+        nameFlag = True
+    else:
+        nameFlag = False
+        break
+
+#validate age
+ageFlag = True
+if int(age) < 18 or int > 65:
+    ageFlag = False
+
+#validate phone number
+phoneFlag = True
+if len(phone_number) != 10:
+    phoneFlag = False
+else:
+    for char in phone_number:
+        if char.isdigit() == False:
+            phoneFlag = False
+            break
+
+print("Validation Results:")
+
+if nameFlag == True:
+    print("Name: Valid (contains only letters and spaces)")
+else:
+    print("Name: Invalid (contains only letters and spaces )")
+
+if ageFlag == True:
+    print(f"Age: Valid ({age} year old)")
+    print("Age: Valid (%d year old)" % int(age))
+else:
+    print("Age: Invalid (less then 18 or more than 65)")
+
+if phoneFlag == True:
+    print("Phone: Valid (10-digit number)")
+else:
+    print("Phone: Invalid (not 10-digit number)")
+
+print("Formatted Information:")
+print("Name: %s" % name.upper())
+#print(name.upper(), name.lower(), name.title(), name.capitalize)
+if int(age) >= 18 and int(age) <= 30:
+    print("Age Group: Young Adult (18-30)")
+else:
+    print("Age Group: not Young Adult")
+print(F"Phone: +66{phone_number}")
--- assignments/week10/string-manipulate.py
+++ assignments/week10/string-manipulate.py
@@ -47,3 +47,23 @@ Transformations:
 USE: len(), split(), count(), upper(), lower(), title(), slicing operations
 
 """
+#Character Analysis:
+text = input("Enter text: ")
+text = "The Quick Brown Fox Jumps Over The Lazy Dog"
+
+print("=== TEXT ANALYSIS REPORT ===")
+print("Charater Analysis: ")
+print("- Total charaters: %d (with spaces), %d (without spaces)" % 
+(len(text), text.count(' ')))
+
+vowels = text.count('a') + text.count('A') + text.count('e') + text.count('E') + text.count('i') + text.count('I')
++ text.count('o') + text.count('O') + text.count('u') + text.count('U')
+
+vowelsString = ""
+for char in text:
+    if char in ['a','e','i','o','u','A','E','I','O','U']:
+        y += char + ", "
+print("vowels: %d (%s)" %(vowels,vowelsString[0, -2]))
+print(f"- Consonants: {len(text - text.count('')-vowels)}")
+
+print(len(text.split()))
